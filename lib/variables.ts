b7b14c5e874d959b5ffa3cc3bad14da065a1362e
Shivam <157960855+shivam-raj12@users.noreply.github.com>
2025-09-09 10:51:56 +0530

    Environment value error fix

--- lib/variables.ts
+++ lib/variables.ts
@@ -1,15 +1,8 @@
 // Appwrite Configuration
 export const APPWRITE_ENDPOINT = 'https://fra.cloud.appwrite.io/v1';
-// export const APPWRITE_PROJECT_ID = '68bc4c5c0009543fc730'
-// export const APPWRITE_DATABASE_ID = "68bc4d0a003dd90b13a0"
-// export const APPWRITE_APP_STORE_TABLE_ID = "app_store"
-// export const APPWRITE_PROFILE_TABLE_ID = "profile"
-// export const APPWRITE_MESSAGES_TABLE_ID = "messages"
-// export const APPWRITE_BLOGS_TABLE_ID = "blogs"
-
-export const APPWRITE_PROJECT_ID = process.env.APPWRITE_PROJECT_ID ?? "";
-export const APPWRITE_DATABASE_ID = process.env.APPWRITE_DATABASE_ID ?? "";
-export const APPWRITE_APP_STORE_TABLE_ID = process.env.APPWRITE_APP_STORE_TABLE_ID ?? "";
-export const APPWRITE_PROFILE_TABLE_ID = process.env.APPWRITE_PROFILE_TABLE_ID ?? "";
-export const APPWRITE_MESSAGES_TABLE_ID = process.env.APPWRITE_MESSAGES_TABLE_ID ?? "";
-export const APPWRITE_BLOGS_TABLE_ID = process.env.APPWRITE_BLOGS_TABLE_ID ?? "";
+export const APPWRITE_PROJECT_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID ?? "";
+export const APPWRITE_DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID ?? "";
+export const APPWRITE_APP_STORE_TABLE_ID = process.env.NEXT_PUBLIC_APPWRITE_APP_STORE_TABLE_ID ?? "";
+export const APPWRITE_PROFILE_TABLE_ID = process.env.NEXT_PUBLIC_APPWRITE_PROFILE_TABLE_ID ?? "";
+export const APPWRITE_MESSAGES_TABLE_ID = process.env.NEXT_PUBLIC_APPWRITE_MESSAGES_TABLE_ID ?? "";
+export const APPWRITE_BLOGS_TABLE_ID = process.env.NEXT_PUBLIC_APPWRITE_BLOGS_TABLE_ID ?? "";
